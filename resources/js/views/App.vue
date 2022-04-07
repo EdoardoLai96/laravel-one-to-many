3483e9e2bd3984d134a9fd259c0879cc7ae48ea2
Edoardo Lai <edoardolai96@gmail.com>
2022-04-07 14:45:36 +0200

    Inizializzazione Repo

---
+++ resources/js/views/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Sito in Manutenzione</div>
+
+                    <div class="card-body">
+                        Torna a trovarci
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: 'App',
+       components: { }
+    }
+</script>
---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.layouts.base')
+
+@section('content')
+    <div class="container">
+    </div>
+@endsection
